ff8fde8f2733f278169b59f8650d89cfe225d036
blinkseb <blinkseb@xbmc.org>
2011-01-27 17:18:49 +0100

    added: support for dependencies

--- buildbot.bat
+++ buildbot.bat
@@ -1,9 +1,11 @@
-@echo on
+@echo off
+
+set current_path=%CD%
 
 set SLN="addon.sln"
 set RELEASE="Release"
-
-set current_path=%CD%
+SET WGET="%current_path%\bin\wget"
+SET ZIP="%current_path%\bin\7za"
 
 set addon_dir=%1
 if $%addon_dir%$ == $$ (
@@ -22,10 +24,8 @@ if $%output_dir%$ == $$ (
   goto end
 )
 
-if not exist %output_dir% (
-  echo Error: output directory does not exist.
-  goto end
-)
+if exist "%output_dir%" rmdir "%output_dir%" /Q /S
+md "%output_dir%"
 
 cd %addon_dir%
 if not exist "windows" (
@@ -50,24 +50,54 @@ cd "windows"
 
 REM download dependencies
 if not exist "dependencies" goto nodeps
+if not exist "dependencies\scripts" goto nodeps
+if not exist "dependencies\scripts\dependencies.txt" goto nodeps
 
 cd "dependencies"
+set DEP_DIR=%CD%
+set TMP_DIR=%DEP_DIR%\scripts\temp
+set DL_DIR=%DEP_DIR%\scripts\download
+set LIB_DIR=%DEP_DIR%\lib
+set BIN_DIR=%DEP_DIR%\bin
+set INC_DIR=%DEP_DIR%\include
+
+if exist "%TMP_DIR%" rmdir "%TMP_DIR%" /Q /S
+if exist "%DL_DIR%" rmdir "%DL_DIR%" /Q /S
+if exist "%LIB_DIR%" rmdir "%LIB_DIR%" /Q /S
+if exist "%BIN_DIR%" rmdir "%BIN_DIR%" /Q /S
+if exist "%INC_DIR%" rmdir "%INC_DIR%" /Q /S
+
+md "%TMP_DIR%"
+md "%DL_DIR%"
+md "%LIB_DIR%"
+md "%BIN_DIR%"
+md "%INC_DIR%"
 
 echo Downloading dependencies...
 
+call "%current_path%\dlextract.bat"
+call "scripts\copy_deps.bat"
 
 REM copy dependencies
-xcopy "bin\*" "..\..\addon\" /EXCLUDE:exclude.txt
-cd ..
+echo Copy dependencies...
 
-REM todo
+if exist "%DEP_DIR%\dependencies.txt" (
+  xcopy "%BIN_DIR%\*" "..\..\addon\" /E /Q /I /Y /EXCLUDE:"%DEP_DIR%\exclude.txt"
+) else (
+  xcopy "%BIN_DIR%\*" "..\..\addon\" /E /Q /I /Y
+)
+
+if exist "%TMP_DIR%" rmdir "%TMP_DIR%" /Q /S
+if exist "%DL_DIR%" rmdir "%DL_DIR%" /Q /S
+
+cd ..
 
 :nodeps
 
 REM build project
 if not exist %SLN% (
   echo Error: %SLN% is missing.
-  goto end
+  goto clean
 )
 
 echo Building addons...
@@ -75,11 +105,11 @@ REM clean the solution
 %VS% %SLN% /clean %RELEASE%
 
 REM build the solution
-%VS% %SLN% /build %RELEASE% /out out.log
+%VS% %SLN% /build %RELEASE%
 
 if errorlevel 1 (
   echo Error: compilation failed
-  goto end
+  goto clean
 )
 
 cd ..
@@ -87,13 +117,20 @@ cd ..
 echo Copying built addons...
 
 cd "addon"
-set dir=%CD%
 
+set dir=%CD%
 cd %current_path%
 
 xcopy "%dir%\*" %output_dir% /E /Q /I /Y
 
 echo All done.
+
+:clean
+if not $%DEP_DIR%$ == $$ (
+  if exist "%LIB_DIR%" rmdir "%LIB_DIR%" /Q /S
+  if exist "%BIN_DIR%" rmdir "%BIN_DIR%" /Q /S
+  if exist "%INC_DIR%" rmdir "%INC_DIR%" /Q /S
+)
 
 :end
 cd %current_path%
---
+++ dlextract.bat
@@ -0,0 +1,26 @@
+@echo off
+
+set current=%CD%
+set DEP_FILE=%CD%\scripts\dependencies.txt
+cd %DL_DIR%
+
+for /F "eol=; tokens=1,2" %%f in (%DEP_FILE%) do (
+  
+  echo Downloading %%f %%g
+  if not exist %%f (
+    %WGET% "%%g/%%f"
+  )
+  copy /b "%%f" "%TMP_DIR%"
+)
+
+cd %TMP_DIR%
+
+for /F "eol=; tokens=1,2" %%f in (%DEP_FILE%) do (
+  %ZIP% x %%f
+)
+
+for /F "tokens=*" %%f in ('dir /B "*.tar"') do (
+  %ZIP% x -y %%f
+)
+
+cd "%current%"
